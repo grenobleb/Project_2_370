9deffbc9667fd8a99999a17165f26898b5daee83
kennyg@sunypoly.edu <kennyg@sunypoly.edu>
2024-12-10 23:52:01 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -28,10 +28,31 @@ class Form1(Form1Template):
     self.Pkm.text = self.dropDownPokemon.text
     pokemonData = get_pokemon_data(self.Pkm.text)
     if pokemonData:
-      # To access and display the ID:
-      PkmId = pokemonData["id"]  # This is how you access the 'id' field in the dictionary
-      # Assuming self.PkmId.text is an attribute in your UI element (e.g., a label in Tkinter or PyQt)
-      self.PkmId.text = str(PkmId)  # Set the text to the Pokémon ID
+
+      # Get the Pokemon's ID
+      tPkmId = pokemonData["_id"]
+      self.PkmId.text = str(tPkmId)
+
+      # Get the Pokemon's category
+      #tPkmCat = pokemonData["_id"]
+      #self.PkmCategory.text = str(tPkmCat)
+
+      # Get the Pokemon's typing
+      tPkmType = pokemonData["type"]
+      self.PkmType.text = str(tPkmType)
+
+      # Get the Pokemon's average weight
+      #tPkmAvgWeight = pokemonData["PkmAvgWeight"]
+      #self.PkmAvgWeight.text = str(tPkmAvgWeight)
+
+      # Get the Pokemon's average height
+      #tPkmAvgHeight = pokemonData["PkmAvgHeight"]
+      #self.PkmAvgHeight.text = str(tPkmAvgHeight)
+
+      # Get the Pokemon's average height
+      #tPkmEntry = pokemonData["PkmEntry"]
+      #self.PkmEntry.text = str(tPkmEntry)
+      
     else:
       self.PkmId.text = "Pokémon not found"
     
