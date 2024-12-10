faa37557645894e815f10d9a329391ea0795cec6
kennyg@sunypoly.edu <kennyg@sunypoly.edu>
2024-12-10 06:59:39 +0000

    Edited Form1, Form1.RowTemplate1, ServerModule1, custom package dependencies and 2 more

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,10 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
 import anvil.server
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
-
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -13,21 +9,14 @@ class Form1(Form1Template):
 
     # Any code you write here will run before the form opens.
 
-  def outlined_button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    pass
-
   def dropDownRegion_change(self, **event_args):
-    """This method is called when an item is selected"""
-    #alert('You selected ' + self.dropDOwnRegion.selected_value)
-    self.selectedRegion.text = self.dropDownRegion.selected_value
-    tempFirstPokemon = app_tables.pokedex.get(region=self.dropDownRegion.selected_value)
-    if tempFirstPokemon:
-        self.firstPokemon.text = tempFirstPokemon['firstPokemon']
+    """Triggered when the user selects and item from the dropdown menu."""
+    region = self.dropDownRegion.selected_value  # Get selected region
+    result = anvil.server.call('getPokemon', region)
+      
+    if result:
+      self.firstPokemon.text = f"First Pokemon: {result['firstPokemon']}"
+      self.lastPokemon.text = f"Last Pokemon: {result['lastPokemon']}"
     else:
-        self.firstPokemon.text = 'No data found'
-    tempLastPokemon = app_tables.pokedex.get(region=self.dropDownRegion.selected_value)
-    if tempLastPokemon:
-        self.lastPokemon.text = tempLastPokemon['lastPokemon']
-    else:
-        self.lastPokemon.text = 'No data found'
+      self.firstPokemon.text = "Region not found."
+      self.lastPokemon.text  = "Region not found."
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,17 +1,20 @@
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
 import anvil.server
+from pymongo import MongoClient
 
-# This is a server module. It runs on the Anvil server,
-# rather than in the user's browser.
-#
-# To allow anvil.server.call() to call functions here, we mark
-# them with @anvil.server.callable.
-# Here is an example - you can replace it with your own:
-#
-# @anvil.server.callable
-# def say_hello(name):
-#   print("Hello, " + name + "!")
-#   return 42
-#
+# Connect to mongodb
+MONGO_URI = "mongodb+srv://localhost:27017@cluster0.mongodb.net/Project2?retryWrites=true&w=majority"
+client = MongoClient(MONGO_URI)
+db = client['Project2']  # Replace with your database name
+pokedex = db['Pokedex']  # Replace with your collection name
+
+@anvil.server.callable
+def getPokemon(region):
+  # Query the MongoDB collection
+  document = pokedex.find_one({"Region": region})
+  if document:
+    return {
+      "firstPokemon": document.get("firstPokemon"),
+      "lastPokemon": document.get("lastPokemon")
+    }
+  else:
+    return None  # Return None if no document matches
