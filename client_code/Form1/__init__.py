f2db7de5c8c1be57f85f7823e186a34d01cbc271
kennyg@sunypoly.edu <kennyg@sunypoly.edu>
2024-12-10 05:44:54 +0000

    Created ServerModule1; Edited settings, Form1.RowTemplate1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,9 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 
 class Form1(Form1Template):
@@ -12,3 +16,18 @@ class Form1(Form1Template):
   def outlined_button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
     pass
+
+  def dropDownRegion_change(self, **event_args):
+    """This method is called when an item is selected"""
+    #alert('You selected ' + self.dropDOwnRegion.selected_value)
+    self.selectedRegion.text = self.dropDownRegion.selected_value
+    tempFirstPokemon = app_tables.pokedex.get(region=self.dropDownRegion.selected_value)
+    if tempFirstPokemon:
+        self.firstPokemon.text = tempFirstPokemon['firstPokemon']
+    else:
+        self.firstPokemon.text = 'No data found'
+    tempLastPokemon = app_tables.pokedex.get(region=self.dropDownRegion.selected_value)
+    if tempLastPokemon:
+        self.lastPokemon.text = tempLastPokemon['lastPokemon']
+    else:
+        self.lastPokemon.text = 'No data found'
