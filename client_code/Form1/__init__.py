9e8301e74eaf0407fc9d8d6b14da02f61fa99e60
kennyg@sunypoly.edu <kennyg@sunypoly.edu>
2024-12-12 03:31:48 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,57 +1,39 @@
 from ._anvil_designer import Form1Template
-from anvil import *
 import anvil.server
+from anvil import *
 
 class Form1(Form1Template):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
+    def __init__(self, **properties):
+        self.init_components(**properties)
+        self.populate_regions()
 
-    # Any code you write here will run before the form opens.
+    def populate_regions(self):
+        # Fetch regions from the server
+        regions = anvil.server.call('get_regions')
+        self.dropDownRegion.items = [(region, region) for region in regions]
 
-  def dropDownRegion_change(self, **event_args):
-    """Triggered when the region dropdown selection changes."""
-    self.selectedRegion.text = self.dropDownRegion.selected_value
-    testSelectedRegion = self.dropDownRegion.selected_value
-    if testSelectedRegion:
-      # Call the server function to get Pokémon for the selected region
-      pokemon_list = anvil.server.call('get_pokemon_by_region', selected_region)
-      
-      # Update the Pokémon dropdown
-      self.dropDownPokemon.items = [(pokemon, pokemon) for pokemon in pokemon_list]
-    else:
-      # Clear the Pokémon dropdown if no region is selected
-      self.dropDownPokemon.items = []
+    def dropDownRegion_change(self, **event_args):
+        # Fetch Pokémon for the selected region
+        selected_region = self.dropDownRegion.selected_value
+        if selected_region:
+            pokemon_list = anvil.server.call('get_pokemon_by_region', selected_region)
+            self.dropDownPokemon.items = [(p['name'], p['id']) for p in pokemon_list]
+            self.dropDownPokemon.enabled = True
+        else:
+            self.dropDownPokemon.items = []
+            self.dropDownPokemon.enabled = False
 
-  def dropDownPokemon_change(self, **event_args):
-    self.Pkm.text = self.dropDownPokemon.text
-    pokemonData = get_pokemon_data(self.Pkm.text)
-    if pokemonData:
-
-      # Get the Pokemon's ID
-      tPkmId = pokemonData["_id"]
-      self.PkmId.text = str(tPkmId)
-
-      # Get the Pokemon's category
-      #tPkmCat = pokemonData["_id"]
-      #self.PkmCategory.text = str(tPkmCat)
-
-      # Get the Pokemon's typing
-      tPkmType = pokemonData["type"]
-      self.PkmType.text = str(tPkmType)
-
-      # Get the Pokemon's average weight
-      #tPkmAvgWeight = pokemonData["PkmAvgWeight"]
-      #self.PkmAvgWeight.text = str(tPkmAvgWeight)
-
-      # Get the Pokemon's average height
-      #tPkmAvgHeight = pokemonData["PkmAvgHeight"]
-      #self.PkmAvgHeight.text = str(tPkmAvgHeight)
-
-      # Get the Pokemon's average height
-      #tPkmEntry = pokemonData["PkmEntry"]
-      #self.PkmEntry.text = str(tPkmEntry)
-      
-    else:
-      self.PkmId.text = "Pokémon not found"
-    
+    def dropDownPokemon_change(self, **event_args):
+        # Fetch details for the selected Pokémon
+        selected_pokemon_id = self.dropDownPokemon.selected_value
+        if selected_pokemon_id:
+            pokemon_details = anvil.server.call('get_pokemon_details', selected_pokemon_id)
+            if pokemon_details:
+                # Populate UI fields with Pokémon details
+                self.Pkm.text = f"Name: {pokemon_details['name']}"
+                self.PkmId.text = f"Id: {pokemon_details['id']}"
+                self.PkmCategory.text = f"Category: {pokemon_details['category']}"
+                self.PkmType.text = f"Type: {pokemon_details['type']}"
+                self.PkmAvgWeight.text = f"Average Weight: {pokemon_details['avg_weight']} kg"
+                self.PkmAvgHeightLabel.text = f"Average Height: {pokemon_details['avg_height']} m"
+                self.PkmEntry.text = f"Dex Entry: {pokemon_details['dex_entry']}"
